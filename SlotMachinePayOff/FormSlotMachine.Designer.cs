fe69437229feb2033e04767783493bbc5f71d9aa
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-12 16:04:30 +0200

    Add project files.

---
+++ SlotMachinePayOff/FormSlotMachine.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SlotMachinePayOff
+{
+    partial class FormSlotMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDisplayResults = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnDisplayResults
+            // 
+            this.BtnDisplayResults.Location = new System.Drawing.Point(59, 60);
+            this.BtnDisplayResults.Name = "BtnDisplayResults";
+            this.BtnDisplayResults.Size = new System.Drawing.Size(162, 58);
+            this.BtnDisplayResults.TabIndex = 0;
+            this.BtnDisplayResults.Text = "Display Results";
+            this.BtnDisplayResults.UseVisualStyleBackColor = true;
+            this.BtnDisplayResults.Click += new System.EventHandler(this.BtnDisplayResults_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(239, 60);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtResult.Size = new System.Drawing.Size(490, 355);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormSlotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnDisplayResults);
+            this.Name = "FormSlotMachine";
+            this.Text = "Slot Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDisplayResults;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ SlotMachinePayOff/FormSlotMachine.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SlotMachinePayOff
+{
+    public partial class FormSlotMachine : Form
+    {
+        public FormSlotMachine()
+        {
+            InitializeComponent();
+        }
+
+        int gains = 0;
+
+        private void BtnDisplayResults_Click(object sender, EventArgs e)
+        {
+            int wheel1, wheel2, wheel3;
+
+            string path = Application.StartupPath + @"\slots.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            wheel1 = Convert.ToInt32(streamReader.ReadLine());
+            wheel2 = Convert.ToInt32(streamReader.ReadLine());
+            wheel3 = Convert.ToInt32(streamReader.ReadLine());
+
+            TxtResult.Text += "DIAL NUMBERS \t         GAIN/LOSS    OUTCOME" + Environment.NewLine;
+            CalculatePayOff(wheel1, wheel2, wheel3);
+
+        }
+
+        private void CalculatePayOff(int wheel1, int wheel2, int wheel3)
+        {
+            int multiplierWheel2 = 0;
+            int multiplierWheel3 = 0;
+
+            int resultWheel1;
+            int resultWheel2;
+            int resultWheel3;
+
+            for (int i = 1; i <= 21; i++)
+            {
+                gains -= 25;
+
+                TxtResult.Text += DisplayText(wheel1, wheel2, wheel3);
+
+                //We find how many times the first wheel reached 10. Those are the times the second wheel made 7 advances.
+                //Then we find how many times the second wheel reach 10. Those are the times the third wheel made 1 advance.
+                //We find where each wheel stopped by finding the reminder of 10 from the total advances.
+                multiplierWheel2 = (wheel1 + 23) / 10;
+                resultWheel1 = (wheel1 + 23) % 10;
+                wheel1 = resultWheel1;
+
+                multiplierWheel3 = (wheel2 + (7 * multiplierWheel2)) / 10;
+                resultWheel2 = (wheel2 + (7 * multiplierWheel2)) % 10;
+                wheel2 = resultWheel2;
+
+                resultWheel3 = (wheel3 + (1 * multiplierWheel3)) % 10;
+                wheel3 = resultWheel3;
+            }
+        }
+
+        private string DisplayText(int score1, int score2, int score3)
+        {
+            string display = "";
+
+            display += score1 + "\t" + score2 + "\t" + score3;
+
+            if (score1 == score2 && score2 == score3)
+            {
+                gains += 500;
+                display += "\t" + gains + "\t JACKPOT" + Environment.NewLine;
+            }
+            else if (score1 == score2 || score1 == score3 || score2 == score3)
+            {
+                gains += 100;
+                display += "\t" + gains + "\t BONUS" + Environment.NewLine;
+            }
+            else
+            {
+                display += "\t" + gains + Environment.NewLine;
+            }
+
+            return display;
+        }
+    }
+}
